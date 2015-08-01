f095d69442aeefd4730137fe579e0f202b336fb6
Julia Bond <julia@yogaed.com>
2015-08-01 11:23:04 -1000

    Added memoize to query selector.

--- memoize.js
+++ memoize.js
@@ -1,13 +1,24 @@
 module.exports = {
-  memoizeElement: memoizeElement()
+  memoizeElement: memoizeElement(),
+  memoizeQuery: memoizeQuery()
 };
 
-function memoizeElement (id) {
+function memoizeElement () {
   var cache = {};
   return function (id) {
     if (!(id in cache)) {
       return cache[id] = document.getElementById(id);
     }
     return cache[id];
+  };
+}
+
+function memoizeQuery () {
+  var cache = {};
+  return function(sel) {
+    if(!(sel in cache)) {
+      return cache[sel] = document.querySelector(sel);
+    }
+    return cache[sel];
   };
 }
